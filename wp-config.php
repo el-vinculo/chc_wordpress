5f473dcf14044cb5c5295df49401056e8b2c512b
unknown <sale@equalinfotech.com>
2019-10-09 18:59:05 +0530

    changes done

--- wp-config.php
+++ wp-config.php
@@ -24,14 +24,15 @@ ini_set('max_execution_time', '300');
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'testsite');
+//define('DB_NAME', 'wp_development');
 
 /** MySQL database username */
 define('DB_USER', 'wp_root');
 //define('DB_USER', 'root');
 
 /** MySQL database password */
-//define('DB_PASSWORD', 'rsi1111');
 define('DB_PASSWORD', 'rsi1111');
+//define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
