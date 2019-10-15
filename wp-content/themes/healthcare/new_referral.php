7a1ae45e57f892d7427baae1ccf0d00c07a0df09
unknown <sale@equalinfotech.com>
2019-10-15 20:21:51 +0530

    new referral changes

--- wp-content/themes/healthcare/new_referral.php
+++ wp-content/themes/healthcare/new_referral.php
@@ -578,7 +578,7 @@ get_header();
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Communication Msg</h4>
+        <h4 class="modal-title">Communication Msg121</h4>
       </div>
       <div class="modal-body">
         <form id="commmsgfrom">
